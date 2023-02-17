9750002a05ff9e21588426152d0df6ae81d89e86
Paulson Mathews <119804104+paulsonmm@users.noreply.github.com>
2023-02-17 09:23:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import NavBar from './Components/Navbar/NavBar';
+import {originals,action, comedy, romance, documentary, horror} from './urls'
+import './App.css'
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar/>
+        <Banner/>
+        <RowPost url={originals} title='Netflix Originals'/>
+        <RowPost url={action} title='Action' isSmall />
+        <RowPost url={horror} title='Horror' isSmall />
+        <RowPost url={comedy} title='Comedy' isSmall />
+        <RowPost url={romance} title='Horror' isSmall />
+        <RowPost url={documentary} title='Documentary' isSmall />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl ='https://api.themoviedb.org/3'
+export const API_KEY ="a8b6cd05ad343907b0e0c6d467c46813"
+export const imageUrl ='https://image.tmdb.org/t/p/original'
